8baf45c0f57f8d8229b6db9a5126dffb93c1e7da
Matthew Dupree <mattjdupree@gmail.com>
2018-10-01 18:40:59 +0200

    Solved

--- Prime1234.hs
+++ Prime1234.hs
@@ -1,30 +1,10 @@
-module Prime1234 where
 
 primes :: [Integer]
 primes =
   sieve (2:[3,5..])
   where
-    sieve (p:xs) = sieve [x | x <- xs, rem x p > 0]
+    sieve (p:xs) = p : sieve [x | x <- xs, mod x p > 0]
 
 
-{--
-  primesToN :: Integer -> [Integer]
-  primesToN n = sieve (2:[3,5..n])
-    where
-    sieve (p:xs)
-       | p*p > n = p:xs
-       | otherwise = p : sieve [x | x <- xs, rem x p > 0]
-  
-  factorsToN :: Integer -> [Integer]
-  factorsToN n =
-    [ x | x<-(primesToN $ truncate $ sqrt $ fromIntegral n), (mod n x)==0 ]
-  
-  largestPrimeFactor :: Integer -> Integer
-  largestPrimeFactor n =
-    let
-      fs = factorsToN n
-    in
-      last fs
-  
-  sol = largestPrimeFactor 600851475143
---}
+main = print (primes!!1234)
+--This prints "10067"
---
+++ makefile
@@ -0,0 +1,2 @@
+prime1234: Prime1234.hs
+	ghc -o Prime1234 Prime1234.hs
